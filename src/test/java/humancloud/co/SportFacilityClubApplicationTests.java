f857bc29b80f3e4db45813a715faf0ff4b1df305
Anil <an.kumar3407@gmail.com>
2024-11-29 10:40:24 +0530

    first commit

---
+++ src/test/java/humancloud/co/SportFacilityClubApplicationTests.java
@@ -0,0 +1,13 @@
+package humancloud.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportFacilityClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
